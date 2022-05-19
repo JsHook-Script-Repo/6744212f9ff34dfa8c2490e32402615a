3f9a93850277b8d635fa93abe25f85a0acd4e910
Aryavrat0 <87088283+Aryavrat0@users.noreply.github.com>
2022-05-20 01:18:06 +0530

    Create rhino.js

---
+++ rhino.js
@@ -0,0 +1,6 @@
+common.hookAllMethods('rikka.appops.ce','诶诶诶',function(param){
+param.setResult(true);
+});
+common.hookAllMethods('rikka.appops.ce','没收门',function(param){
+param.setResult(false);
+});
